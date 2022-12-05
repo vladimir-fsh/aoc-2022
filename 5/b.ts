c09e4d88e9ac3ad7ec12968c00a9341cc7c2c708
Bob <vmh@medicuja.com>
2022-12-05 17:22:58 +0100

    day 5

---
+++ 5/b.ts
@@ -0,0 +1,30 @@
+const res: string[] = await Deno.readTextFile('./input.txt');
+const data: string[] = res.split('\n');
+
+const stacks = {
+  1: ['R', 'G', 'H', 'Q', 'S', 'B', 'T', 'N'],
+  2: ['H', 'S', 'F', 'D', 'P', 'Z', 'J'],
+  3: ['Z', 'H', 'V'],
+  4: ['M', 'Z', 'J', 'F', 'G', 'H'],
+  5: ['T', 'Z', 'C', 'D', 'L', 'M', 'S', 'R'],
+  6: ['M', 'T', 'W', 'V', 'H', 'Z', 'J'],
+  7: ['T', 'F', 'P', 'L', 'Z'],
+  8: ['Q', 'V', 'W', 'S'],
+  9: ['W', 'H', 'L', 'M', 'T', 'D', 'N', 'C']
+}
+
+
+const moveItems = (from, to, count) => {
+  const items = stacks[from].splice(-count);
+  stacks[to].push(...items)
+};
+
+
+
+for (const str of data) {
+  const [count, from, to] = str.match(/\d+/g).map(Number);
+  moveItems(from, to, count);
+}
+
+
+console.log('lsssssa', Object.values(stacks).map(arr => arr[arr.length-1]).join(''));
